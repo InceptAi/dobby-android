9bd1d4f8d6851cfa1e00058be9f19904f2d60361
Vivek Shrivastava <vivek@obiai.tech>
2017-03-30 16:08:56 -0700

    Working on Bandwidth Analyzer

---
+++ Dobby/app/src/main/java/com/inceptai/dobby/speedtest/BandwidthAnalyzer.java
@@ -0,0 +1,13 @@
+package com.inceptai.dobby.speedtest;
+
+/**
+ * Created by vivek on 3/30/17.
+ */
+
+/**
+ * Class contains logic performing bandwidth tests.
+ */
+
+public class BandwidthAnalyzer {
+
+}
