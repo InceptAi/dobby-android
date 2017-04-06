55f399f4d46e4c2d68e3d7c066c133b26e03f373
Arunesh Mishra <aruneshm@gmail.com>
2017-04-06 14:34:56 -0700

    IE changes

--- Dobby/app/src/main/java/com/inceptai/dobby/ai/InferenceEngine.java
+++ Dobby/app/src/main/java/com/inceptai/dobby/ai/InferenceEngine.java
@@ -11,6 +11,28 @@ import ai.api.model.Result;
 public class InferenceEngine {
     private static final String CANNED_RESPONSE = "We are working on it.";
 
+    private static final String APIAI_ACTION_DIAGNOSE_SLOW_INTERNET = "diagnose-slow-internet-action";
+
+    private static final String APIAI_ACTION_SI_STARTING_INTENT_NO =
+            "slow-internet-starting-intent.slow-internet-starting-intent-no";
+
+    private static final String APIAI_ACTION_SI_STARTING_INTENT_CANCEL =
+            "slow-internet-starting-intent.slow-internet-starting-intent-cancel";
+
+    private static final String APIAI_ACTION_SI_STARTING_INTENT_LATER =
+            "slow-internet-starting-intent.slow-internet-starting-intent-later";
+
+    private static final String APIAI_ACTION_SI_STARTING_INTENT_YES_YES_CANCEL =
+            "slow-internet-starting-intent.slow-internet-starting-intent-yes.slow-internet-starting-intent-yes-cancel";
+
+    private static final String APIAI_ACTION_SI_STARTING_INTENT_YES_YES_LATER =
+            "slow-internet-starting-intent.slow-internet-starting-intent-yes.slow-internet-starting-intent-yes-later";
+
+    private static final String APIAI_ACTION_SI_STARTING_INTENT_YES_YES_NO =
+            "slow-internet-starting-intent.slow-internet-starting-intent-yes.slow-internet-starting-intent-yes-no";
+
+    private Action previousAction;
+
     public Action interpretApiAiResult(Result result) {
         // Show user response if any.
         String response = result.getFulfillment().getSpeech();
@@ -18,7 +40,11 @@ public class InferenceEngine {
             response = CANNED_RESPONSE;
         }
 
-        Action action = new Action(response, Action.ActionType.ACTION_NONE);
-        return action;
+        String apiAiAction = result.getAction();
+
+        previousAction = new Action(response, Action.ActionType.ACTION_NONE);
+        return previousAction;
     }
+
+
 }
