a2e615cac0fa55eaa98bf402242591139a126dd4
Vivek Shrivastava <vivek@obiai.tech>
2017-04-08 13:30:18 -0700

    Removing unnecessary public static final from interface def.

--- Dobby/app/src/main/java/com/inceptai/dobby/speedtest/BandwithTestCodes.java
+++ Dobby/app/src/main/java/com/inceptai/dobby/speedtest/BandwithTestCodes.java
@@ -16,12 +16,12 @@ public class BandwithTestCodes {
             BandwidthTestMode.UPLOAD, BandwidthTestMode.CONFIG_FETCH,
             BandwidthTestMode.SERVER_FETCH, BandwidthTestMode.IDLE})
     public @interface BandwidthTestMode {
-        public static final int DOWNLOAD_AND_UPLOAD = 0;
-        public static final int DOWNLOAD = 1;
-        public static final int UPLOAD = 2;
-        public static final int CONFIG_FETCH = 3;
-        public static final int SERVER_FETCH = 4;
-        public static final int IDLE = 5;
+        int DOWNLOAD_AND_UPLOAD = 0;
+        int DOWNLOAD = 1;
+        int UPLOAD = 2;
+        int CONFIG_FETCH = 3;
+        int SERVER_FETCH = 4;
+        int IDLE = 5;
     }
 
     @Retention(RetentionPolicy.SOURCE)
@@ -32,16 +32,16 @@ public class BandwithTestCodes {
             BandwidthTestErrorCodes.ERROR_SOCKET_TIMEOUT, BandwidthTestErrorCodes.ERROR_CONNECTION_ERROR,
             BandwidthTestErrorCodes.ERROR_TEST_INTERRUPTED})
     public @interface BandwidthTestErrorCodes {
-        public static final int ERROR_UNKNOWN = 1;
-        public static final int ERROR_FETCHING_CONFIG = 2;
-        public static final int ERROR_PARSING_CONFIG = 3;
-        public static final int ERROR_FETCHING_SERVER_INFO = 4;
-        public static final int ERROR_PARSING_SERVER_INFO = 5;
-        public static final int ERROR_SELECTING_BEST_SERVER = 6;
-        public static final int ERROR_INVALID_HTTP_RESPONSE = 7;
-        public static final int ERROR_SOCKET_ERROR = 8;
-        public static final int ERROR_SOCKET_TIMEOUT = 9;
-        public static final int ERROR_CONNECTION_ERROR = 10;
-        public static final int ERROR_TEST_INTERRUPTED = 11;
+        int ERROR_UNKNOWN = 1;
+        int ERROR_FETCHING_CONFIG = 2;
+        int ERROR_PARSING_CONFIG = 3;
+        int ERROR_FETCHING_SERVER_INFO = 4;
+        int ERROR_PARSING_SERVER_INFO = 5;
+        int ERROR_SELECTING_BEST_SERVER = 6;
+        int ERROR_INVALID_HTTP_RESPONSE = 7;
+        int ERROR_SOCKET_ERROR = 8;
+        int ERROR_SOCKET_TIMEOUT = 9;
+        int ERROR_CONNECTION_ERROR = 10;
+        int ERROR_TEST_INTERRUPTED = 11;
     }
 }
