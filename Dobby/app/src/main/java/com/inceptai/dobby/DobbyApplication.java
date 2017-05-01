d870d318ab4966447beacadd5245e549e62acf1f
Arunesh Mishra <aruneshm@gmail.com>
2017-05-01 11:07:49 -0700

    Uuid API in DobbyApplication

--- Dobby/app/src/main/java/com/inceptai/dobby/DobbyApplication.java
+++ Dobby/app/src/main/java/com/inceptai/dobby/DobbyApplication.java
@@ -6,8 +6,14 @@ import android.util.Log;
 import com.inceptai.dobby.dagger.DaggerProdComponent;
 import com.inceptai.dobby.dagger.ProdComponent;
 import com.inceptai.dobby.dagger.ProdModule;
+import com.inceptai.dobby.utils.Utils;
 
+import java.util.EnumMap;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static com.inceptai.dobby.utils.Utils.EMPTY_STRING;
 
 /**
  * Created by arunesh on 3/28/17.
@@ -19,6 +25,7 @@ public class DobbyApplication extends Application {
     public static final String WIFIDOC_FLAVOR = "wifidoc";
     public static final String USER_UUID = "userUuid";
     public static final AtomicBoolean USE_FAKES = new AtomicBoolean(false);
+    private final AtomicReference<String> userUuid = new AtomicReference<>();
 
     private ProdComponent prodComponent;
 
@@ -32,6 +39,15 @@ public class DobbyApplication extends Application {
         // Thread.setDefaultUncaughtExceptionHandler(new DobbyThreadpool.DobbyUncaughtExceptionHandler(handler));
     }
 
+    private synchronized void fetchUuid() {
+        String uuid = Utils.readSharedSetting(this, USER_UUID, EMPTY_STRING);
+        if (EMPTY_STRING.equals(uuid)) {
+            uuid = UUID.randomUUID().toString();
+            Utils.saveSharedSetting(this, USER_UUID, uuid);
+        }
+        userUuid.set(uuid);
+    }
+
     public ProdComponent getProdComponent() {
         return prodComponent;
     }
@@ -42,5 +58,9 @@ public class DobbyApplication extends Application {
 
     public static boolean isWifiDocFlavor() {
         return BuildConfig.FLAVOR.equals(WIFIDOC_FLAVOR);
+    }
+
+    public String getUserUuid() {
+        return userUuid.get();
     }
 }
