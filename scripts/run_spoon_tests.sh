90c973a06d6fcd698d973c389e20a4d0a6eec785
Vivek Shrivastava <vivek@obiai.tech>
2017-05-14 18:21:47 -0700

    Correcting run_spoon_tests to send proper email

--- scripts/run_spoon_tests.sh
+++ scripts/run_spoon_tests.sh
@@ -10,15 +10,13 @@ echo "Starting gradle test" > /tmp/gradle.log
 
 if [ $? -gt 0 ]; then
 	echo "BUILD FAILED"
-	SUBJECT="Gradle Build Failed for WifiDoc"
 	BODY="Gradle build failed: See http://dobby1743.duckdns.org:5187/ for UI test results"
 	ATTACHMENT="/tmp/gradle.log"
-	echo ${BODY}| mail -s ${SUBJECT} -a ${ATTACHMENT} $VIVEK_EMAIL $ARUNESH_EMAIL
+	echo ${BODY}| mail -s "Gradle Build Failed for WifiDoc" -a ${ATTACHMENT} $VIVEK_EMAIL $ARUNESH_EMAIL
 else
 	echo "BUILD SUCCEEDED"
-	SUBJECT="Gradle Build Success for WifiDoc"
 	BODY="Gradle build succeeded: See http://dobby1743.duckdns.org:5187/ for UI screenshots"
-	echo ${BODY}| mail -s ${SUBJECT} $VIVEK_EMAIL $ARUNESH_EMAIL
+	echo ${BODY}| mail -s "Gradle Build Success for WifiDoc" $VIVEK_EMAIL $ARUNESH_EMAIL
 fi
 
 exit 0
