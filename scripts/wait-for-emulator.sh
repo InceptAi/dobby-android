ced60faf3de17641bc729458f429a26d5bcba07c
viveks9 <vivek@obiai.tech>
2017-05-17 11:43:01 -0700

    Cleaning up tests stuff

--- scripts/wait-for-emulator.sh
+++ scripts/wait-for-emulator.sh
@@ -9,7 +9,8 @@ failcounter=0
 timeout_in_sec=360
 
 until [[ "$bootanim" =~ "stopped" ]]; do
-  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+  #bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+  bootanim=`adb wait-for-device shell getprop init.svc.bootanim 2>&1 &`
   if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
     || "$bootanim" =~ "running" ]]; then
     let "failcounter += 1"
@@ -19,6 +20,7 @@ until [[ "$bootanim" =~ "stopped" ]]; do
       exit 1
     fi
   fi
+  echo "Bootanim is:" "$bootanim"
   sleep 1
 done
 
