2af27999720f3df199988553921d70d44d273f8f
Vivek Shrivastava <vivek@obiai.tech>
2017-05-25 14:00:57 -0700

    Automate scripting changes

--- scripts/automate_spoon_test_dobby.sh
+++ scripts/automate_spoon_test_dobby.sh
@@ -8,19 +8,22 @@ cat << EOF
         -h            display this help and exit
 		-r 			  check for git updates after interval and run tests if new updates (default: run once)
         -d|--testdir  test dir (/tmp/SpoonTests) by default
+        -p|--gmplayerpath  Genymotion player path
 		-i|--interval min interval between tests in mins(default is 10 mins)
 		-g|--github   github clone repo (default:dobby-android -- https://github.com/InceptAi/dobby-android.git)
+		-f|--emulator_file_list   github clone repo (default:dobby-android -- https://github.com/InceptAi/dobby-android.git)
         -v            verbose mode. Can be used multiple times for increased verbosity.
 EOF
 }
 
 # Reset all variables that might be set
-test_dir="/tmp/SpoonTests"
-github_repo="https://github.com/InceptAi/dobby-android.git"
+genymotion_player_path=
+TEST_DIR="/tmp/SpoonTests"
+GITHUB_REPO="https://github.com/InceptAi/dobby-android.git"
 test_interval=0
 verbose=0 # Variables to be evaluated as shell arithmetic should be initialized to a default or validated beforehand.
 repeat_tests=0
-
+emulator_list_file=
 verbose_echo () {
     if [ $verbose -gt 0 ]; then
         echo $1
@@ -40,7 +43,7 @@ while :; do
             ;;
         -d|--testdir)    # Takes an option argument, ensuring it has been specified.
             if [ -n "$2" ]; then
-                test_dir=$2
+                TEST_DIR=$2
                 shift
             else
                 printf 'ERROR: "-d|--testdir" requires a non-empty option argument.\n' >&2
@@ -49,7 +52,7 @@ while :; do
             ;;
         -g|--github)       # Takes an option argument, ensuring it has been specified.
             if [ -n "$2" ]; then
-                github_repo=$2
+                GITHUB_REPO=$2
                 shift
             else
                 printf 'ERROR: "--github|-b" requires a non-empty option argument.\n' >&2
@@ -65,6 +68,17 @@ while :; do
                 exit 1
             fi
             ;;
+        -f|--emulatorfile)       # Takes an option argument, ensuring it has been specified.
+            if [ -n "$2" ]; then
+                emulator_list_file=$2
+                shift
+            else
+                printf 'ERROR: "--emulatorfile|-f" requires a non-empty option argument.\n' >&2
+                exit 1
+            fi
+            ;;
+ 
+
         -v|--verbose)
             verbose=$((verbose + 1)) # Each -v argument adds 1 to verbosity.
             ;;
@@ -86,10 +100,10 @@ done
 
 
 one_time_setup () {
-	rm -rf $test_dir
-	mkdir -p $test_dir
-	cd $test_dir
-	git clone $github_repo
+	rm -rf $TEST_DIR
+	mkdir -p $TEST_DIR
+	cd $TEST_DIR
+	git clone $GITHUB_REPO
 	cd $GITHUB_REPO_NAME
 }
 
@@ -113,11 +127,11 @@ check_git () {
 }
 
 # https://github.com/InceptAi/dobby-android.git
-GITHUB_REPO_NAME=`echo $gitrepo | rev | cut -d "/" -f 1 | rev | cut -d "." -f 1`
-PATH_TO_REPO_DIR=$test_dir/$GITHUB_REPO_NAME
-GRADLEW_PATH=$PATH_TO_REPO_DIR/Dobby
+GITHUB_REPO_NAME=`echo $GITHUB_REPO | rev | cut -d "/" -f 1 | rev | cut -d "." -f 1`
+PATH_TO_REPO_DIR="$TEST_DIR/$GITHUB_REPO_NAME"
+GRADLEW_PATH="$PATH_TO_REPO_DIR/Dobby"
+DOBBY_SERVER_HOME="$PATH_TO_REPO_DIR/server"
 
-EMULATOR_LIST_FILE=$2
 ARUNESH_EMAIL="arunesh@obiai.tech"
 VIVEK_EMAIL="vivek@obiai.tech"
 BODY="Test Email"
@@ -126,7 +140,6 @@ REL_PATH_DEBUG_APK="app/build/outputs/apk/app-wifidoc-debug.apk"
 REL_PATH_INSTRUMENTATION_APK="app/build/outputs/apk/app-wifidoc-debug-androidTest.apk"
 MIN_API_LEVEL_TO_USE_G_INSTALL_OPTION=23
 OUTPUT_PATH="/home/vivek/Work/dobby-android/Dobby/app/build/spoon/wifidoc"
-DOBBY_SERVER_HOME="/home/vivek/Work/dobby-android/server"
 
 uninstall_app () {
     #uninstall the apks from the device
@@ -235,103 +248,125 @@ wait_for_git_changes () {
 		git_status=check_git
 	done
 }
+
+run_emulator_tests () {
+
+    echo "List of emulators:"
+    cat $emulator_list_file
+    should_report_failure=0
+
+
+    rm -rf $DOBBY_SERVER_HOME/spoon/wifidoc
+    clean_slate
+    num_lines=`wc -l $emulator_list_file | cut -d ' ' -f1`
+    for i in $(seq 1 $num_lines); do
+   	    line=`sed -n ${i}p $emulator_list_file`
+        emulator_info="$line"
+        if [ -z "$emulator_info" ]; then
+            continue
+        fi
+        
+        is_commented=`echo $emulator_info | grep "#"`
+        if [ ! -z "$is_commented" ]; then
+            echo "Emulator $emulator_info"
+            continue
+        fi
+        
+        echo "Running test for id $emulator_info"
+        emulator_id=`echo $emulator_info | cut -d " " -f 1`
+        api_level=`echo $emulator_info | cut -d " " -f 2`
+        echo "emulator id, api level" $emulator_id $api_level 
+        
+        #Start the emulator
+        $GENYMOTION_HOME/player --vm-name $emulator_id &	
+        EMULATOR_PID=$!
+        echo "Started emulator with pid:" $EMULATOR_PID
+
+        #Slight delay before we check
+        sleep 5
+
+        #Wait for it to start
+        wait_for_emulator
+
+        #Sleep for a few secs	
+        sleep 15
+
+        #Uninstall the app
+        uninstall_app
+
+        #Install the app
+        install_app $api_level
+
+        #Run the test
+        ./gradlew spoonWifidocDebugAndroidTest --stacktrace >>  /tmp/gradle.log
+        
+        #Store the output in a diff location
+        mkdir -p $DOBBY_SERVER_HOME/spoon/wifidoc/$api_level
+        cp -r $OUTPUT_PATH/debug/* $DOBBY_SERVER_HOME/spoon/wifidoc/$api_level/
+        
+        if [ $? -gt 0 ]; then
+            should_report_failure=1
+        fi
+
+        #Repeat for different emulators
+        uninstall_app
+        
+        #Kill the emulator
+        kill $EMULATOR_PID
+        sleep 10
+        
+        clean_slate
+        echo "Done sleeping, onto the next one"
+    done
+}
+
+
+build_test_targets () {
+    cd $GRADLEW_PATH
+    echo "Starting gradle build" > /tmp/gradle.log
+    ./gradlew clean build assembleAndroidTest >> /tmp/gradle.log
+    if [ $? -gt 0 ]; then
+        notify_failure
+        exit 1
+    fi
+}
 	 
+#Starting tests
 #set up the dirs
 one_time_setup
 
-#wait till git changes
-wait_for_git_changes 
+while [ $repeat_tests -gt 0 ]; do 
 
-#git changed, do a pull and run tests
-git_pull
+    #git changed, do a pull and run tests
+    git_pull
 
-#run the tests
-cd $test_dir/dobby-android/Dobby
+    #build the tests
+    build_test_targets
 
-
-echo "Starting gradle build" > /tmp/gradle.log
-./gradlew clean build assembleAndroidTest >> /tmp/gradle.log
-
-
-if [ $? -gt 0 ]; then
-    notify_failure
-    exit 1
-fi
-
-#Check emulator list exists
-if [ ! -f $EMULATOR_LIST_FILE ]; then
-	echo "$EMULATOR_LIST_FILE does not exist. Exiting"
-	exit 0
-fi
-
-echo "List of emulators:"
-cat $EMULATOR_LIST_FILE
-should_report_failure=0
-
-rm -rf $DOBBY_SERVER_HOME/spoon/wifidoc
-clean_slate
-num_lines=`wc -l $EMULATOR_LIST_FILE | cut -d ' ' -f1`
-for i in $(seq 1 $num_lines); do
-   	line=`sed -n ${i}p $EMULATOR_LIST_FILE`
-    emulator_info="$line"
-    if [ -z "$emulator_info" ]; then
-        continue
+    #Check emulator list exists
+    if [ ! -f $emulator_list_file ]; then
+        echo "$emulator_list_file does not exist. Exiting"
+        exit 0
     fi
-    is_commented=`echo $emulator_info | grep "#"`
-    if [ ! -z "$is_commented" ]; then
-        echo "Emulator $emulator_info"
-        continue
+
+    if [ ! -n $genymotion_player_path ]; then
+        echo "Genymotion player path not defined, can't run emulator tests"
+        exit 0
     fi
-    echo "Running test for id $emulator_info"
-	emulator_id=`echo $emulator_info | cut -d " " -f 1`
-	api_level=`echo $emulator_info | cut -d " " -f 2`
-	echo "emulator id, api level" $emulator_id $api_level 
-	#Start the emulator
-	$GENYMOTION_HOME/player --vm-name $emulator_id &	
-	EMULATOR_PID=$!
-	echo "Started emulator with pid:" $EMULATOR_PID
 
-	#Slight delay before we check
-	sleep 5
 
-	#Wait for it to start
-	wait_for_emulator
+    #run the tests
+    run_emulator_tests
 
-	#Sleep for a few secs	
-	sleep 15
 
-	#Uninstall the app
-	uninstall_app
-
-	#Install the app
-	install_app $api_level
-
-	#Run the test
-	./gradlew spoonWifidocDebugAndroidTest --stacktrace >>  /tmp/gradle.log
-	
-	#Store the output in a diff location
-	mkdir -p $DOBBY_SERVER_HOME/spoon/wifidoc/$api_level
-	cp -r $OUTPUT_PATH/debug/* $DOBBY_SERVER_HOME/spoon/wifidoc/$api_level/
-	
-	if [ $? -gt 0 ]; then
-		should_report_failure=1
-	fi
-
-	#Repeat for different emulators
-	uninstall_app
-	
-	#Kill the emulator
-	kill $EMULATOR_PID
-	sleep 10
-	
-	clean_slate
-	echo "Done sleeping, onto the next one"
+    if [ $should_report_failure -gt 0 ]; then
+        notify_failure
+        exit 1
+    else 
+        notify_success
+        exit 0
+    fi
+    
+    #Wait for more git changes
+    wait_for_git_changes 
 done
-
-if [ $should_report_failure -gt 0 ]; then
-	notify_failure
-	exit 1
-else 
-	notify_success
-	exit 0
-fi
