173dbe1cd27f766c9d30a7d91aedf39faa93fb7f
Arunesh Mishra <aruneshm@gmail.com>
2017-06-07 10:05:06 -0700

    notifications javascript work.

--- ExpertChatWebapp/functions/index.js
+++ ExpertChatWebapp/functions/index.js
@@ -11,4 +11,45 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 
-functions.database.ref('/').onWrite(event => {});
+functions.database.ref('/notifications/messages/{pushId}')
+  .onWrite(event => {
+
+    const message = event.data.current.val();
+    if (message == null) {
+        console.log('null message received.');
+        return;
+    }
+    console.log('message ' + message);
+
+    const senderUid = message.from;
+    const receiverUid = message.to;
+    const fcmIdPath = message.fcmIdPath;
+    const promises = [];
+
+    if (senderUid == receiverUid) {
+        // if sender is receiver, don't send notification
+        promises.push(event.data.current.ref.remove());
+        return Promise.all(promises);
+    }
+
+    const getInstanceIdPromise = admin.database().ref(`${fcmIdPath}/fcmToken`).once('value');
+    return Promise.all([getInstanceIdPromise]).then(results => {
+        const instanceId = results[0].val();
+        console.log('notifying ' + receiverUid + ' about ' + message.body + ' from ' + senderUid);
+
+        const payload = {
+            notification: {
+                title: message.title,
+                body: message.body,
+            }
+        };
+
+        admin.messaging().sendToDevice(instanceId, payload)
+            .then(function (response) {
+                console.log("Successfully sent message:", response);
+            })
+        .catch(function (error) {
+            console.log("Error sending message:", error);
+        });
+    }
+});
