8e5b3c63513d3e4ae884f2313df62261a64a7e61
Arunesh Mishra <aruneshm@gmail.com>
2017-06-22 18:23:34 -0700

    Expertchat notification revamp. User database

--- ExpertChat/app/src/main/java/com/inceptai/expertchat/NotificationRecents.java
+++ ExpertChat/app/src/main/java/com/inceptai/expertchat/NotificationRecents.java
@@ -1,9 +1,15 @@
 package com.inceptai.expertchat;
 
+import android.util.Log;
+
 import com.raizlabs.android.dbflow.annotation.Database;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
 
+import java.util.List;
 import java.util.Map;
+
+import static com.inceptai.expertchat.Utils.EMPTY_STRING;
+import static com.inceptai.expertchat.Utils.TAG;
 
 /**
  * Created by arunesh on 6/21/17.
@@ -18,16 +24,77 @@ public class NotificationRecents {
 
     }
 
-    public void saveIncomingNotification(Map<String, String> notificationData) {
+    /**
+     *
+     * @param notificationData
+     * @return UserData object, or null on bad data.
+     */
+    public UserData saveIncomingNotification(Map<String, String> notificationData) {
         String fromUuid = notificationData.get("source");
         String appFlavor = notificationData.get("flavor");
+
+        if (fromUuid == null || fromUuid.isEmpty()) {
+            Log.i(TAG, "Empty UUID, unable to save incoming notification.");
+            return null;
+        }
+        UserData userData = UserDataBackend.fetchUser(fromUuid);
+        if (userData == null) {
+            userData = new UserData();
+            userData.userUuid = fromUuid;
+        }
+
+        if (appFlavor != null && !appFlavor.isEmpty()) {
+            userData.appFlavor = appFlavor;
+        }
+
+        userData.setFreshnessTimestampMs(System.currentTimeMillis());
+        switch (userData.interactionType) {
+            case UserData.INTERACTION_UNKNOWN:
+                userData.interactionType = UserData.USER_NOTIF_RESPONSE_PENDING;
+                break;
+            case UserData.ONGOING_CHAT_EXPERT_RESPONSE_SENT:
+                userData.interactionType = UserData.ONGOING_CHAT_USER_NOTIF_PENDING;
+                break;
+            case UserData.ONGOING_CHAT_USER_NOTIF_PENDING:
+                break;
+            case UserData.USER_NOTIF_RESPONSE_PENDING:
+                break;
+        }
+        userData.save();
+        return userData;
     }
 
     public void notificationSent(ChatNotification chatNotification) {
+        String userUuid = chatNotification.to;
+        if (userUuid == null || userUuid.isEmpty()) {
+            Log.e(TAG, "Unable to record notification for null or empty user ID.");
+            return;
+        }
 
+        UserData userData = UserDataBackend.fetchUser(userUuid);
+        if (userData == null) {
+            userData = new UserData();
+            userData.userUuid = userUuid;
+        }
+
+        userData.appFlavor = Utils.getFlavorFromFcmIdPath(chatNotification.fcmIdPath);
+        userData.buildType = Utils.getBuildTypeFromFcmIdPath(chatNotification.fcmIdPath);
+
+        userData.setFreshnessTimestampMs(System.currentTimeMillis());
+        switch (userData.interactionType) {
+            case UserData.INTERACTION_UNKNOWN:
+                userData.interactionType = UserData.ONGOING_CHAT_EXPERT_RESPONSE_SENT;
+                break;
+            case UserData.ONGOING_CHAT_EXPERT_RESPONSE_SENT:
+                break;
+            case UserData.ONGOING_CHAT_USER_NOTIF_PENDING:
+                userData.interactionType = UserData.ONGOING_CHAT_EXPERT_RESPONSE_SENT;
+                break;
+            case UserData.USER_NOTIF_RESPONSE_PENDING:
+                userData.interactionType = UserData.ONGOING_CHAT_EXPERT_RESPONSE_SENT;
+                break;
+        }
+        userData.save();
     }
 
-    UserData fetchUser(String userUuid) {
-        return SQLite.select().from(UserData.class).where(UserData_Table.userUuid.eq(userUuid)).querySingle();
-    }
 }
--- ExpertChat/app/src/main/java/com/inceptai/expertchat/UserData.java
+++ ExpertChat/app/src/main/java/com/inceptai/expertchat/UserData.java
@@ -1,6 +1,7 @@
 package com.inceptai.expertchat;
 
 import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.ColumnIgnore;
 import com.raizlabs.android.dbflow.annotation.PrimaryKey;
 import com.raizlabs.android.dbflow.annotation.Table;
 import com.raizlabs.android.dbflow.structure.BaseModel;
@@ -12,9 +13,10 @@ import com.raizlabs.android.dbflow.structure.BaseModel;
 @Table(database = NotificationRecents.class)
 public class UserData extends BaseModel {
 
+    public static final int INTERACTION_UNKNOWN = 1;
     public static final int USER_NOTIF_RESPONSE_PENDING = 5001;
     public static final int ONGOING_CHAT_EXPERT_RESPONSE_SENT = 5002;
-    public static final int ONGOING_CHAT_USER_NOTIF_PENDING = 5002;
+    public static final int ONGOING_CHAT_USER_NOTIF_PENDING = 5003;
 
     @Column
     @PrimaryKey
@@ -28,6 +30,16 @@ public class UserData extends BaseModel {
 
     @Column
     String appFlavor;
+
+    @Column
+    String buildType;
+
+    @ColumnIgnore
+    int lastNotificationId;
+
+    public UserData() {
+        interactionType = INTERACTION_UNKNOWN;
+    }
 
     public String getUserUuid() {
         return userUuid;
@@ -59,5 +71,17 @@ public class UserData extends BaseModel {
 
     public void setAppFlavor(String appFlavor) {
         this.appFlavor = appFlavor;
+    }
+
+    public String getBuildType() {
+        return buildType;
+    }
+
+    public void setBuildType(String buildType) {
+        this.buildType = buildType;
+    }
+
+    public boolean hasBuildTypeAndFlavor() {
+        return buildType != null && !buildType.isEmpty() && appFlavor != null && !appFlavor.isEmpty();
     }
 }
