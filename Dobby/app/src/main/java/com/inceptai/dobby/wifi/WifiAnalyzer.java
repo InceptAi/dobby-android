b49477817f1cf766ea017164a572b3bb7aa77093
viveks9 <vivek@obiai.tech>
2017-06-29 20:03:19 -0700

    Fixing wifi expert

--- Dobby/app/src/main/java/com/inceptai/dobby/wifi/WifiAnalyzer.java
+++ Dobby/app/src/main/java/com/inceptai/dobby/wifi/WifiAnalyzer.java
@@ -7,6 +7,7 @@ import android.content.IntentFilter;
 import android.net.DhcpInfo;
 import android.net.NetworkInfo;
 import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.support.annotation.Nullable;
@@ -137,6 +138,10 @@ public class WifiAnalyzer {
 
     public DhcpInfo getDhcpInfo() {
         return wifiManager.getDhcpInfo();
+    }
+
+    public List<WifiConfiguration> getWifiConfiguration() {
+        return wifiManager.getConfiguredNetworks();
     }
 
     public WifiState getWifiState() {
