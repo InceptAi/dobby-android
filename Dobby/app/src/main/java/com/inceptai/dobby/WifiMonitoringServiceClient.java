79a132e515b7e10d1bf0747c968babc21158d70f
viveks9 <vivek@obiai.tech>
2017-08-13 08:31:28 -0700

    WifiServiceClient

---
+++ Dobby/app/src/main/java/com/inceptai/dobby/WifiMonitoringServiceClient.java
@@ -0,0 +1,14 @@
+package com.inceptai.dobby;
+
+import android.content.Context;
+
+import com.inceptai.wifimonitoringservice.WifiMonitoringService;
+
+/**
+ * Created by vivek on 8/11/17.
+ */
+
+public class WifiMonitoringServiceClient {
+    private WifiMonitoringService wifiMonitoringService;
+    private Context context;
+}
